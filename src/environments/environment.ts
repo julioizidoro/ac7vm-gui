deaaef7e3327a2f1195aa5f0ec0762872898e2ec
BROGNOLI\naidion.brovedan <brovedan@gmail.com>
2019-10-28 11:00:14 -0300

    alteracao endereço api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost/api/',
-  baseApiUrl: 'http://localhost/api/'
+  aseUrl: 'http://ac7vm.com.br/ac7vm-api/api/',
+  baseApiUrl: 'http://ac7vm.com.br/ac7vm-api/api/'
 };
 
 /*
